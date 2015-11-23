8e178e8aebd4b2782f45563ab49c8c823cd46008
danie <danie@10.50.131.157>
2015-11-23 12:23:22 +0100

    Gui update (danny)

--- src/queing/Quiz.java
+++ src/queing/Quiz.java
@@ -9,10 +9,12 @@ import java.awt.Color;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JButton;
+import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
 
 /**
@@ -20,7 +22,7 @@ import javax.swing.SwingUtilities;
  * @author Daniel
  */
 public class Quiz {
-    Queing_GUI gui = this.gui;
+
     private ArrayList<String[]> quiz = new ArrayList<String[]>();
     private String[] q1 = {"What is 2 + 3", "4", "1", "2", "5", "5"};
     private String[] q2 = {"How many hours are there a day?", "20", "21", "15", "24", "24"};
@@ -30,11 +32,16 @@ public class Quiz {
     private String[] q6 = {"10 + 3 = ?", "2", "3333", "4444", "13", "13"};
     private boolean running = true;
     private int numOfQsAns = 0;
-    private int score = 0;
-
+    private String answer;
+    private int points;
+    private Random gen = new Random();
     private String[] curQ;
 
-    public void addQnA() {
+    public Quiz() {
+        addQnA();
+    }
+
+    final public void addQnA() {
         quiz.add(q1);
         quiz.add(q2);
         quiz.add(q3);
@@ -43,98 +50,138 @@ public class Quiz {
         quiz.add(q6);
     }
 
-    public boolean isDone() {
-        return numOfQsAns > 3;
-
-    }
-
-    public void score(Queing_GUI q) {
-        if (isDone()) {
-            try {
-                q.getjLabelQScore().setText("Congratulations!\n You got " + (score) + " pt.");
-            } catch (NullPointerException e) {
-
-            }
+    boolean setAnswers(JTextArea TextArea, JButton Button1, JButton Button2,
+            JButton Button3, JButton Button4) {
+        if(quiz.isEmpty()) {
+            return true;
         }
-    }
-
-    public void updateQs(Queing_GUI a) {
-        if (numOfQsAns < 5) {
-            Collections.shuffle(quiz);
-            curQ = quiz.get(0);
-            a.getjLabelQuestion().setText("" + curQ[0]);
-            a.getjButtonA1().setText("" + curQ[1]);
-            a.getjButtonA2().setText("" + curQ[2]);
-            a.getjButtonA3().setText("" + curQ[3]);
-            a.getjButtonA4().setText("" + curQ[4]);
-            numOfQsAns++;
-        } else {
-            isDone();
-        }
+        int quizLength = quiz.size();
+        int QuestionUsedInt = gen.nextInt(quizLength);
+        String[] QuestionUsed = quiz.get(QuestionUsedInt);
+        TextArea.setText(QuestionUsed[0]);
+        Button1.setText(QuestionUsed[1]);
+        Button2.setText(QuestionUsed[2]);
+        Button3.setText(QuestionUsed[3]);
+        Button4.setText(QuestionUsed[4]);
+        answer = QuestionUsed[5];
+        quiz.remove(QuestionUsedInt);
+        return false;
     }
     
-    public boolean isCorrect(int x) {
-        boolean isCorrect = false;
-        if (curQ[x].equalsIgnoreCase(curQ[5])) {
-            isCorrect = true;
-            quiz.remove(curQ);
+    void checkAnswer(String text) {
+        if(text.equals(answer)){
+            // You get points
+            System.out.println("Correct");
+            points += 100;
         }
-        return isCorrect;
-    }
-    
-    public void sleep(int x) {
-        try {
-            TimeUnit.MILLISECONDS.sleep(x);
-        } catch (InterruptedException ex) {
-            
+        else{
+            System.out.println("Wrong");
         }
     }
     
-    public void updateScore(Queing_GUI q) {
-        q.getjLabelCurQScore().setText("Score: " + score);
+    public int getPoints() {
+        return points;
     }
     
-    public void buttonEvent(int i, Queing_GUI q, JButton jB) {
-        if (isCorrect(i) && isDone()) {
-            jB.setBackground(Color.green);
-            q.getjPanelGame().update(q.getjPanelGame().getGraphics());
-            sleep(600);
-            jB.setBackground(new JButton().getBackground());
-            q.getjPanelGame().setVisible(false);
-            q.getjPanelScore().setVisible(true);
-            score += 10;
-            score(q);
-            q.setRunning(false);
-        } else if (isCorrect(i)) {
-            jB.setBackground(Color.green);
-            q.getjPanelGame().update(q.getjPanelGame().getGraphics());
-            
-            //q.getjPanelGame().setBackground(Color.green);
-           // q.getjPanelGame().update(q.getjPanelGame().getGraphics());
-            sleep(600);
-            jB.setBackground(new JButton().getBackground());
-           // q.getjPanelGame().setBackground(Color.white);
-            score += 10;
-            updateQs(q);
-            
-        } else if (!isCorrect(i) && isDone()) {
-            jB.setBackground(Color.red);
-            q.getjPanelGame().update(q.getjPanelGame().getGraphics());
-            sleep(600);
-            jB.setBackground(new JButton().getBackground());
-            q.getjPanelGame().setVisible(false);
-            q.getjPanelScore().setVisible(true);
-            score(q);
-        } else {
-            jB.setBackground(Color.red);
-            q.getjPanelGame().update(q.getjPanelGame().getGraphics());
-            sleep(600);
-            jB.setBackground(new JButton().getBackground());
-            updateQs(q);
-        }
-        updateScore(q);
+    String getPointsAsString() {
+        return ""+ getPoints();
     }
-    
     
 
+//    public boolean isDone() {
+//        return numOfQsAns > 3;
+//
+//    }
+//
+//    public void score(Queing_GUI q) {
+//        if (isDone()) {
+//            try {
+//                q.getjLabelQScore().setText("Congratulations!\n You got " + (score) + " pt.");
+//            } catch (NullPointerException e) {
+//
+//            }
+//        }
+//    }
+//
+//    public void updateQs(Queing_GUI a) {
+//        if (numOfQsAns < 5) {
+//            Collections.shuffle(quiz);
+//            curQ = quiz.get(0);
+//            a.getjLabelQuestion().setText("" + curQ[0]);
+//            a.getjButtonA1().setText("" + curQ[1]);
+//            a.getjButtonA2().setText("" + curQ[2]);
+//            a.getjButtonA3().setText("" + curQ[3]);
+//            a.getjButtonA4().setText("" + curQ[4]);
+//            numOfQsAns++;
+//        } else {
+//            isDone();
+//        }
+//    }
+//    
+//    public boolean isCorrect(int x) {
+//        boolean isCorrect = false;
+//        if (curQ[x].equalsIgnoreCase(curQ[5])) {
+//            isCorrect = true;
+//            quiz.remove(curQ);
+//        }
+//        return isCorrect;
+//    }
+//    
+//    public void sleep(int x) {
+//        try {
+//            TimeUnit.MILLISECONDS.sleep(x);
+//        } catch (InterruptedException ex) {
+//            
+//        }
+//    }
+//    
+//    public void updateScore(Queing_GUI q) {
+//        q.getjLabelCurQScore().setText("Score: " + score);
+//    }
+//    
+//    public void buttonEvent(int i, Queing_GUI q, JButton jB) {
+//        if (isCorrect(i) && isDone()) {
+//            jB.setBackground(Color.green);
+//            q.getjPanelGame().update(q.getjPanelGame().getGraphics());
+//            sleep(600);
+//            jB.setBackground(new JButton().getBackground());
+//            q.getjPanelGame().setVisible(false);
+//            q.getjPanelScore().setVisible(true);
+//            score += 10;
+//            score(q);
+//            q.setRunning(false);
+//        } else if (isCorrect(i)) {
+//            jB.setBackground(Color.green);
+//            q.getjPanelGame().update(q.getjPanelGame().getGraphics());
+//            
+//            //q.getjPanelGame().setBackground(Color.green);
+//           // q.getjPanelGame().update(q.getjPanelGame().getGraphics());
+//            sleep(600);
+//            jB.setBackground(new JButton().getBackground());
+//           // q.getjPanelGame().setBackground(Color.white);
+//            score += 10;
+//            updateQs(q);
+//            
+//        } else if (!isCorrect(i) && isDone()) {
+//            jB.setBackground(Color.red);
+//            q.getjPanelGame().update(q.getjPanelGame().getGraphics());
+//            sleep(600);
+//            jB.setBackground(new JButton().getBackground());
+//            q.getjPanelGame().setVisible(false);
+//            q.getjPanelScore().setVisible(true);
+//            score(q);
+//        } else {
+//            jB.setBackground(Color.red);
+//            q.getjPanelGame().update(q.getjPanelGame().getGraphics());
+//            sleep(600);
+//            jB.setBackground(new JButton().getBackground());
+//            updateQs(q);
+//        }
+//        updateScore(q);
+
+    
+
+    
+    
+    
 }
